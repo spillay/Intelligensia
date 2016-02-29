e5f0059893d8cadf818c9a0eaebed0ee1d84e2b1
suresh <suresh@workspaceafrica.com>
2016-02-29 16:18:57 +0200

    Create Simple Button

--- app/src/main/java/com/dsleng/android/intelligensia/MainActivityFragment.java
+++ app/src/main/java/com/dsleng/android/intelligensia/MainActivityFragment.java
@@ -1,15 +1,18 @@
 package com.dsleng.android.intelligensia;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
 
     public MainActivityFragment() {
     }
@@ -17,6 +20,14 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        Button bt = (Button)v.findViewById(R.id.check_button);
+        bt.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(v.getContext(),"SP HELLO",Toast.LENGTH_SHORT).show();
     }
 }
